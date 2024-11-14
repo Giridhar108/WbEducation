530cfb39b9af079c0d39f10f86399c48269d626e
German Fomenko <gfomenko@idscan.net>
2024-11-14 12:54:13 +0500

    .

--- src/app.js
+++ src/app.js
@@ -1,4 +1,31 @@
-console.log('act');
+class Account {
+    constructor(balance) {
+        this.balance = balance;
+    }
+    getBalance() {
+        console.log(this.balance);
+    }
+    getMoney(value) {
+        this.balance = this.balance - value;
+    }
+    putMoney(value) {
+        this.balance = this.balance + value;
+    }
+}
+class DebitAccount extends Account {
+    getBalance() {
+        console.log(this.balance);
+    }
+}
+class CreditAccount extends Account {
+    getBalance() {
+        console.log(this.balance);
+    }
+}
+const debitAccount = new DebitAccount(108);
+const creditAccount = new CreditAccount(1008);
+debitAccount.getBalance();
+creditAccount.getBalance();
 // Разработать типизированную систему управления банковскими счетами, 
 // где каждый счет может быть дебетовым или кредитным с различными условиями обслуживания. 
 // В системе должны быть: интерфейс Account, который описывает общие свойства банковского счета, 
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,51 @@
-console.log(
-    'act'
-)
+type AccountType = 'debit' | 'credit'
 
 interface Account {
     balance: number,
-    getBalance(): number,
-
+    getBalance(): void,
+    getMoney(value: number): void,
+    putMoney(value: number): void
 }
+
+class Account implements Account {
+    constructor(balance: number) {
+        this.balance = balance;
+    }
+
+    getBalance(): void {
+        console.log(this.balance);
+    }
+
+    getMoney(value: number): void {
+        this.balance = this.balance - value;
+    }
+
+    putMoney(value: number): void {
+        this.balance = this.balance + value;
+    }
+}
+
+class DebitAccount extends Account {
+    getBalance(): void {
+        console.log(this.balance);
+    }
+}
+
+class CreditAccount extends Account {
+    getBalance(): void {
+        console.log(this.balance);
+    }
+
+    setPercentage(value: number): void {
+        this.balance = value;
+    }
+}
+
+const debitAccount = new DebitAccount(108)
+const creditAccount = new CreditAccount(1008)
+
+debitAccount.getBalance()
+creditAccount.getBalance()
 
 // Разработать типизированную систему управления банковскими счетами, 
 // где каждый счет может быть дебетовым или кредитным с различными условиями обслуживания. 
